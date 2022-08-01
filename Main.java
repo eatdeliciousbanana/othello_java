23dce13090cd2d08aeb71d824ebc101312bd3912
eatdeliciousbanana <93957952+eatdeliciousbanana@users.noreply.github.com>
2022-08-02 00:15:08 +0900

    動作確認

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-class Main {
-    public static void main(String args[]) {
-        System.out.println("hello");
+import java.io.IOException;
+
+public class Main {
+    public static void main(String... arg) throws IOException, InterruptedException {
+        Othello game = new Othello();
+        while (!game.getEnd()) {
+            game.display();
+            game.put(game.putPosition()[0]);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+            }
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        }
+        game.display();
+
     }
 }
